fdbfe399965989d47857a46d18854cf74f29ce7e
mOji <mhce92@gmail.com>
2020-09-21 12:39:42 +0330

    create SettingsFragment

--- app/src/main/java/com/example/twogamesinone/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/twogamesinone/fragments/SettingsFragment.java
@@ -7,60 +7,70 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.example.twogamesinone.R;
+import com.example.twogamesinone.model.Settings;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText mPlayerOneName;
+    private EditText mPlayerTwoName;
+    private Switch mSwitchPoints;
+    private Button mSaveButton;
+    private Settings mSettings = Settings.getInstance();
 
     public SettingsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        findViews(view);
+        setListeners();
+        return view;
+    }
+
+    private void findViews(View view) {
+        mPlayerOneName = view.findViewById(R.id.edit_text_first_player);
+        mPlayerTwoName = view.findViewById(R.id.edit_text_second_player);
+        mSwitchPoints = view.findViewById(R.id.switch_point_view);
+        mSaveButton = view.findViewById(R.id.button_save);
+    }
+
+    private void setListeners() {
+        mSwitchPoints.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                mSettings.setShowPoints(b);
+            }
+        });
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] names = new String[2];
+                names[0] = mPlayerOneName.getText().toString();
+                names[1] = mPlayerTwoName.getText().toString();
+                for (int i = 0; i < 2; i++) {
+                    if (names[i].length() == 0)
+                        names[i] = String.format(getString(R.string.player), i + 1);
+                }
+                mSettings.setNames(names);
+                Toast.makeText(getActivity(), R.string.changes_saved, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
